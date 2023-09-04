02e71c82390c1cfc9ad46cf8801dcd5dfe544c79
HanbitKang314ano <symphomic@gmail.com>
2023-09-04 18:18:36 -0500

    remove feeder-react-feedback to check

--- my-app/src/pages/feedbacks.js
+++ my-app/src/pages/feedbacks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Feedback from 'feeder-react-feedback'; // import Feedback component
+// import Feedback from 'feeder-react-feedback'; // import Feedback component
 import "../styles/feedbacks.css";
 
 const Feedbacks = () => {
@@ -26,12 +26,14 @@ return (
 			}}
 			>
 			<h1>You can write your feedback about my website here!</h1>
+			{/*
 			<Feedback
 				projectId="64a5e4017b39c80002d2503f"
 				postSubmitButtonMsg= "Thank you for your input!"
 				email= "true"
 				emailRequired= "true"
 			/>
+			*/}
 		</div>
 	</div>
 );
